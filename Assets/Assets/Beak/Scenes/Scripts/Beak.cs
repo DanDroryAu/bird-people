1565e6d0cef7db05284f725f5bd9d3c4eeb76132
Steph Vallance <44055497+steph603@users.noreply.github.com>
2022-09-16 23:34:46 +1000

    Snappy boi

---
+++ Assets/Assets/Beak/Scenes/Scripts/Beak.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Beak : MonoBehaviour
+{
+    [SerializeField] float maxBeakAngle = 10f;
+    [SerializeField] private float beakClosedAngle = 4f;
+    [SerializeField] private float beakVelocity = 0.1f;
+    [SerializeField] private float topBeakRotation = 0f;
+    [SerializeField] private float bottomBeakRotation = 0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform topBeak = transform.Find("TopBeak");
+    }
+
+    // Update is called once per frame
+        void FixedUpdate()
+        {
+            topBeakRotation = transform.Find("TopBeak").transform.rotation.eulerAngles.z;
+            bottomBeakRotation = transform.Find("BottomBeak").transform.rotation.eulerAngles.z;
+
+            if (Input.GetMouseButton(0))
+            {
+                if(topBeakRotation >= beakClosedAngle) {
+                    transform.Find("TopBeak").transform.Rotate(0, 0, -beakVelocity);
+                    transform.Find("BottomBeak").transform.Rotate(0, 0, beakVelocity);
+                }
+            }
+            else if (!Input.GetMouseButton(0))
+            {
+                if (topBeakRotation <= maxBeakAngle) {
+                    transform.Find("TopBeak").transform.Rotate(0, 0, beakVelocity);
+                    transform.Find("BottomBeak").transform.Rotate(0, 0, -beakVelocity);
+                }
+            }
+
+        }
+    }
+    
---
+++ Assets/Assets/Beak/Scenes/Scripts/BeakParts.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class BeakParts : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.gameObject.tag == "Trash")
+        {
+            Debug.Log("Yummy trash");
+            
+            // reference of pivot point of wholeAssBeak
+            // if angle of beak parts > something
+            // function to lock the trash component to the pivotPoint
+            
+        } 
+    }
+}
